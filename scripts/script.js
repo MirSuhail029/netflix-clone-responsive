be01e23e2afe8b19e22d5dc87705e80a93669f93
Suhail <mirsuhail029@gmail.com>
2023-05-02 00:11:50 +0530

    made some changes to hero section

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-const langContainer=document.getElementsByClassName("language-container");
+const langContainer = document.getElementsByClassName("language-container");
 
-let flag=true;
-for(const container of langContainer){
-    const dropDownList=container.getElementsByClassName("dropdown");
-    for(const dropDown of dropDownList){
-        container.addEventListener("click",function(){
-            if (flag===true){
-                dropDown.setAttribute("class","dropdown");
-                flag=false;
-            }else if(flag===false){
-                dropDown.setAttribute("class","dropdown hidden");
-                flag=true;
-            }
-        });
-    };   
-};
+let flag = true;
+for (const container of langContainer) {
+  const dropDownList = container.getElementsByClassName("dropdown");
+  for (const dropDown of dropDownList) {
+    container.addEventListener("click", function () {
+      if (flag === true) {
+        dropDown.setAttribute("class", "dropdown");
+        flag = false;
+      } else if (flag === false) {
+        dropDown.setAttribute("class", "dropdown hidden");
+        flag = true;
+      }
+    });
+  }
+}
